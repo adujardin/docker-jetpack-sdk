9f65d66760872f397468e0b6dae5d6a09a0cee2b
Aymeric DUJARDIN <aymeric.dujardin@stereolabs.com>
2020-08-04 14:09:28 +0200

    Update auvidea patch version

--- auvidea_patch.sh
+++ auvidea_patch.sh
@@ -2,9 +2,9 @@
 # JP44 DP for Nano JN30B
 
 cd /home/user/nvidia/nvidia_sdk/
-wget https://auvidea.eu/download/firmware/JN30/B/JN30B_Jetpack_4_4.tar.bz2
-tar -xf JN30B_Jetpack_4_4.tar.bz2
-tar -xf JN30B_Jetpack_4_4/kernel_out.tar.bz2
+wget https://www.auvidea.eu/download/firmware/JN30/B/JN30B_Jetpack4_4_v3_1.tar.bz2
+tar -xf JN30B_Jetpack*.tar.bz2
+tar -xf JN30B_Jetpack4_4_v3_1/kernel_out.tar.bz2
 cp kernel_out/* JetPack_4.4_DP_Linux_DP_JETSON_NANO/Linux_for_Tegra/ -R
 cd /home/user/nvidia/nvidia_sdk/JetPack_4.4_DP_Linux_DP_JETSON_NANO/Linux_for_Tegra/
 sudo ./apply_binaries.sh
